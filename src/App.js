ecff8255d136af7967fc9685e011c05e98b96fd9
Monishamuthu <monumuthu7@gmail.com>
2020-01-24 18:17:49 +0530

    header added
    header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./styles.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {/* <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2> */}
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            {/*  <Route path="/cart" component={Cart}/> */}
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
