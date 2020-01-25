bcd758562a98e44f2541f86d8e1bdfce7a589ec3
Monishamuthu <monumuthu7@gmail.com>
2020-01-25 18:22:11 +0530

    Card Added
    Card Added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import cartReducer from "./reducers/CartReducer";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const store = createStore(cartReducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
--- src/pages/home/view/Home.js
+++ src/pages/home/view/Home.js
@@ -1,12 +1,61 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+const styles = theme => ({
+  card: {
+    minWidth: 275
+  }
+});
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <div>Home page</div>;
+    const { classes } = this.props;
+    let itemList = this.props.items.map(item => {
+      return (
+        <Card className={classes.card} variant="outlined">
+          <CardContent>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+            >
+              {item.title}
+            </Typography>
+          </CardContent>
+        </Card>
+      );
+    });
+
+    return (
+      /*   <React.Fragment> */
+      <Container maxWidth="sm">
+        <h3 style={{ textAlign: "center" }}>Our items</h3>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexWrap: "wrap"
+          }}
+        >
+          {" "}
+          {itemList}
+        </div>
+      </Container>
+      /*  </React.Fragment> */
+    );
   }
 }
+const mapStateToProps = state => {
+  return {
+    items: state.items
+  };
+};
 
-export default Home;
+export default withStyles(styles)(connect(mapStateToProps)(Home));
